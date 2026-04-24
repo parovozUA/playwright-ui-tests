d0e1eeace968d20d68fbcb754b68a112bdb49135
parovozUA <carbitnis@gmail.com>
2026-04-24 02:46:36 +0200

    refactor(checkout): use helper and mark invalid cases as xfail

--- tests/test_checkout.py
+++ tests/test_checkout.py
@@ -1,11 +1,12 @@
 import pytest
 from playwright.sync_api import expect
 
-from data.CheckoutCases import CheckoutCase, CHECKOUT_CASES
+from data.CheckoutCases import CHECKOUT_CASES
 from data.CheckoutForm import CheckoutForms
 from pages.CartPage import CartPage
 from pages.CheckoutPage import CheckoutPage
 from pages.InventoryPage import InventoryPage
+from utils.helpers import assert_error_message
 
 
 @pytest.mark.parametrize(
@@ -13,7 +14,13 @@ from pages.InventoryPage import InventoryPage
     CHECKOUT_CASES,
     ids=[case.name for case in CHECKOUT_CASES]
 )
-def test_checkout_form_validation(logged_in_page, case: CheckoutCase):
+def test_checkout_form_validation(logged_in_page, case):
+    if case.name == "all fields spaces":
+        pytest.xfail("Form does not trim spaces")
+
+    if case.name == "long values":
+        pytest.xfail("Form does not validate long values")
+
     inventory_page = InventoryPage(logged_in_page)
     cart_page = CartPage(logged_in_page)
     checkout_page = CheckoutPage(logged_in_page)
@@ -35,7 +42,7 @@ def test_checkout_form_validation(logged_in_page, case: CheckoutCase):
     if case.should_pass:
         expect(checkout_page.title_span).to_have_text("Checkout: Overview")
     else:
-        checkout_page.assert_error_message(case.error_message)
+        assert_error_message(checkout_page.error_message, case.error_message)
 
 
 def test_checkout_successful_flow(logged_in_page):
@@ -80,7 +87,7 @@ def test_checkout_error_message_can_be_closed(logged_in_page):
 
     checkout_page.fill_checkout_form(CheckoutForms.EMPTY_FIRST_NAME)
     checkout_page.continue_button.click()
-    expect(checkout_page.error_message_container).to_be_visible()
+    expect(checkout_page.error_message).to_be_visible()
 
     checkout_page.error_button.click()
-    expect(checkout_page.error_message_container).not_to_be_visible()
+    expect(checkout_page.error_message).not_to_be_visible()
